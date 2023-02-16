73237c77e305d6a52ac9f76a6ac2d4b2b7d9128a
Sharan Thakur <sharandt19@live.com>
2023-02-16 12:07:29 +0530

    checking if search body is valid

--- lib/src/helpers/helper_functions.dart
+++ lib/src/helpers/helper_functions.dart
@@ -14,3 +14,19 @@ bool isBodyValid(Map<String, dynamic> jsonMap) {
 
   return true;
 }
+
+bool isSearchBodyValid(Map<String, dynamic> jsonMap) {
+  if (jsonMap['count'] == null || jsonMap['count'] == 0) {
+    return false;
+  }
+
+  if (jsonMap['quotes'] == null) {
+    return false;
+  }
+
+  if (jsonMap['quotes'] is List && jsonMap['quotes'].isEmpty) {
+    return false;
+  }
+
+  return true;
+}
