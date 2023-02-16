fd9085f6953edbf4d8575642668d74fb374818e8
Sharan Thakur <sharandt19@live.com>
2023-02-16 12:08:00 +0530

    added models for stock search as well as re-format old

--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -1,7 +1,77 @@
+class StockSearchResult {
+  final int count;
+  final List<StockQuote> stockQuotes;
+
+  StockSearchResult(
+    this.stockQuotes,
+    this.count,
+  );
+
+  factory StockSearchResult.fromJson(Map<String, dynamic> jsonMap) {
+    final List<StockQuote> quotes = List.from(jsonMap['quotes'].map((e) => StockQuote.fromJson(e)));
+    return StockSearchResult(quotes, jsonMap['count']);
+  }
+
+  @override
+  String toString() {
+    return 'StockSearchResult{count: $count, stockQuotes: $stockQuotes}';
+  }
+}
+
+class StockQuote {
+  final String exchange, exchangeDisplay;
+  final String symbol;
+  final String longName, shortName;
+  final String sector;
+  final String industry;
+
+  StockQuote({
+    required this.symbol,
+    required this.exchange,
+    required this.exchangeDisplay,
+    required this.shortName,
+    required this.longName,
+    required this.sector,
+    required this.industry,
+  });
+
+  factory StockQuote.fromJson(Map<String, dynamic> jsonMap) {
+    return StockQuote(
+      symbol: jsonMap['symbol'],
+      exchange: jsonMap['exchange'],
+      exchangeDisplay: jsonMap['exchDisp'],
+      longName: jsonMap['longname'],
+      shortName: jsonMap['shortname'],
+      sector: jsonMap['sector'],
+      industry: jsonMap['industry'],
+    );
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is StockQuote &&
+          runtimeType == other.runtimeType &&
+          exchange == other.exchange &&
+          exchangeDisplay == other.exchangeDisplay &&
+          symbol == other.symbol;
+
+  @override
+  int get hashCode =>
+      exchange.hashCode ^ exchangeDisplay.hashCode ^ symbol.hashCode;
+
+  @override
+  String toString() {
+    return 'StockQuote{exchange: $exchange, exchangeDisplay: $exchangeDisplay, symbol: $symbol, longName: $longName, shortName: $shortName, sector: $sector, industry: $industry}';
+  }
+}
+
 class StockData {
   final String ticker;
   final String shortName, longName;
-  final double regularMarketChangePercent, regularMarketChange, regularMarketPrice;
+  final double regularMarketChangePercent,
+      regularMarketChange,
+      regularMarketPrice;
   final int? marketCap;
 
   StockData({
@@ -14,8 +84,7 @@ class StockData {
     this.marketCap,
   });
 
-  factory StockData.fromJson(Map<String, dynamic> jsonMap) =>
-      StockData(
+  factory StockData.fromJson(Map<String, dynamic> jsonMap) => StockData(
         ticker: jsonMap['symbol'],
         shortName: jsonMap['shortName'],
         longName: jsonMap['longName'],
@@ -25,9 +94,12 @@ class StockData {
         marketCap: jsonMap['marketCap'],
       );
 
-  factory StockData.fromJsonWithName(String ticker, Map<String, dynamic> jsonMap) =>
+  factory StockData.fromJsonWithName(
+    String ticker,
+    Map<String, dynamic> jsonMap,
+  ) =>
       StockData(
-        ticker: ticker,
+        ticker: ticker.toUpperCase(),
         shortName: jsonMap['shortName'],
         longName: jsonMap['longName'],
         regularMarketChangePercent: jsonMap['regularMarketChangePercent'],
@@ -44,9 +116,9 @@ class StockData {
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-          other is StockData &&
-              runtimeType == other.runtimeType &&
-              ticker == other.ticker;
+      other is StockData &&
+          runtimeType == other.runtimeType &&
+          ticker == other.ticker;
 
   @override
   int get hashCode => ticker.hashCode;
