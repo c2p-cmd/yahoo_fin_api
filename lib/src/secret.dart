c2c9320d1de1d5fa4eb580cddf253c7535c3a22b
Sharan Thakur <sharandt19@live.com>
2023-02-16 12:08:07 +0530

    added new urls

--- lib/src/secret.dart
+++ lib/src/secret.dart
@@ -1,1 +1,7 @@
 const apiUrl = 'https://query1.finance.yahoo.com/v7/finance/quote?symbols=';
+String searchUrl(String symbol) =>
+    'https://query2.finance.yahoo.com/v1/finance/search?q=$symbol&region=IN&newsCount=0';
+Uri searchUri(String symbol) => Uri.parse(searchUrl(symbol));
+
+const baseUrl = 'query2.finance.yahoo.com';
+const path = '/v1/finance/search';
